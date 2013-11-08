9532b667daef785018ddcb1dcad14e928c8f7401
nscavell <nick.scavelli@gmail.com>
2013-11-08 17:36:33 -0500

    Add logging when server successfully binds for template app

--- versions/2.1/template/server.js
+++ versions/2.1/template/server.js
@@ -17,13 +17,22 @@
 
 var vertx = require('vertx');
 var container = require('vertx/container');
+var console = require('vertx/console');
 //TODO: Use this once https://github.com/vert-x/mod-lang-js/issues/4 is fixed
 //var ip = container.env['OPENSHIFT_VERTX_IP'] || '127.0.0.1';
 //var port = parseInt(container.env['OPENSHIFT_VERTX_IP'] || 8080);
 var ip = container.env.get('OPENSHIFT_VERTX_IP') || '127.0.0.1';
 var port = parseInt(container.env.get('OPENSHIFT_VERTX_PORT') || 8080);
 
+console.log('Binding server on ' + ip + ':' + port);
+
 vertx.createHttpServer().requestHandler(function(req) {
   var file = req.path() === '/' ? 'index.html' : req.path();
   req.response.sendFile('webroot/' + file);
-}).listen(port, ip);
+}).listen(port, ip, function(err) {
+    if (!err) {
+      console.log('Successfully listening on ' + ip + ':' + port);
+    } else {
+      console.log('Could not bind to ' + ip + ':' + port + '. Error: ' + err);
+    }
+});
